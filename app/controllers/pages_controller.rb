36deaa0323cb6a2617cc231753f4e168f4ed1d13
Ishmael Khalid <Ishmaelkhalid@icloud.com>
2015-07-03 16:21:36 -0400

    Added about, blog, portfolio, and services page view, action, route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+  
   def home
   end
+  
+  def about
+  end
+
+  def portfolio
+  end
+  
+  def blog
+  end
+  
+  def services
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/about' => 'pages#about'
+  get '/portfolio' => 'pages#portfolio'
+  get '/blog' => 'pages#blog'
+  get '/services' => 'pages#services'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
